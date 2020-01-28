c001b20b6478e1e52744da8de09cb7c24a60577d
Devansh Dalal <devansh.dalal@thoughtspot.com>
2020-01-28 20:41:34 +0530

    Updates

---
+++ precache-manifest.0298285e8941bb30ebb1786f7e0af3b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0e787b47ec492986512336a2fb8acf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1489f4524faa7ffbc49",
+    "url": "/static/css/main.db631142.chunk.css"
+  },
+  {
+    "revision": "ddb54f0d36612a5e7c9e",
+    "url": "/static/js/2.0d8590f7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0d8590f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1489f4524faa7ffbc49",
+    "url": "/static/js/main.3b8e071e.chunk.js"
+  },
+  {
+    "revision": "e6266a3616a5209f88b0",
+    "url": "/static/js/runtime-main.008e8ac0.js"
+  },
+  {
+    "revision": "a7ff79684546a09242d17cb10e3e5483",
+    "url": "/static/media/cpp-icon.a7ff7968.png"
+  },
+  {
+    "revision": "a4a03c169c0992448d09bc393d4abec2",
+    "url": "/static/media/go-icon.a4a03c16.png"
+  },
+  {
+    "revision": "404230e7c1b23e26bb368812976c6861",
+    "url": "/static/media/html-icon.404230e7.png"
+  },
+  {
+    "revision": "02ed4e6e3b480f2012111c30cfa9ac31",
+    "url": "/static/media/java-icon.02ed4e6e.png"
+  },
+  {
+    "revision": "ccf5b0481329e8ec3a7b15455acb1167",
+    "url": "/static/media/jupyter-icon.ccf5b048.png"
+  },
+  {
+    "revision": "7d0f0b2c4dc1ce85101480b06b2dffc8",
+    "url": "/static/media/php-icon.7d0f0b2c.png"
+  },
+  {
+    "revision": "1ebd114cee6503f2c41dddd92705a5ce",
+    "url": "/static/media/python-icon.1ebd114c.png"
+  }
+]);
